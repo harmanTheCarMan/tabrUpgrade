61fcfa8bc219264e70ab7b30c6e632d9302681cb
Harman Singh <harmanlearns@gmail.com>
2016-11-30 15:21:07 -0800

    setup authentication, added passport, passport-local to package.json, finished authentication routes in app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express')
-var path = require('path')
-var favicon = require('serve-favicon')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const passport = require( './authentication/passport' ).passport
 
-var index = require('./routes/index')
-var users = require('./routes/users')
+const index = require('./routes/index')
+const users = require('./routes/users')
+const tabs = require('./routes/tabs')
+const tasks = require('./routes/tasks')
 
-var app = express()
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -21,13 +25,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use( session({ secret: 'slartibartfast', saveUninitialized: true, resave: false, cookie: { secure: false } }))
+app.use( passport.initialize() )
+app.use( passport.session() )
 
 app.use('/', index)
 app.use('/users', users)
+app.use('/tasks', tasks)
+app.use('/tabs', tabs)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found')
+  const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
