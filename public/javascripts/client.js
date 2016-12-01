944e9e1e0820750c4e031d4dc61c1e0fe851813e
Harman Singh <harmanlearns@gmail.com>
2016-11-30 16:21:40 -0800

    Setup views, added tabr.png, and client.js code

---
+++ public/javascripts/client.js
@@ -0,0 +1,19 @@
+$(document).ready( () => {
+  $('.task input[type=checkbox]').change( () => {
+    var url = '/tasks/' +
+      ( $(this).is( ':checked' ) ? 'complete' : 'uncomplete' ) +
+      'Task'
+    var id = $(this).data( 'id' )
+
+    $.post( url, { id: id } )
+  })
+
+  $('input.task-description').on( 'keypress', (event) => {
+    if( event.charCode === 13 ) {
+      var id = $(this).data( 'id' )
+      var value = $(this).val()
+
+      $.post( '/tasks/update', { id: id, value: value } )
+    }
+  })
+})
